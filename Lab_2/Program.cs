f640ccabd2bc86465d985274d0a981f2ef313e2b
panshinmv <m4ks563@gmail.com>
2024-10-28 11:15:02 +0300

    Lab_2_3

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-
+using System.Numerics;
+using System.Collections.Generic;
+using System.Collections;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearRegression;
 
 
 interface ICreate
 {
     public void Create();
 }
-abstract class Figure
+abstract class Figure : IComparable<Figure>,IPrint
 {
     public abstract double Area();
-
+    
+    public int CompareTo(Figure other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        return this.Area().CompareTo(other.Area());
+    }
+    public abstract void Print();
 }
+
 
 class Check
 {
@@ -31,7 +46,7 @@ class Check
     }
 }
 
-class Rectangle : Figure,IPrint, ICreate
+class Rectangle : Figure, ICreate
 {
    public void Create()
     {
@@ -62,13 +77,13 @@ class Rectangle : Figure,IPrint, ICreate
     {
         return "Высота:" + this.height.ToString() + " Ширина: " + this.width.ToString() + " Площадь: " + this.Area().ToString();
     }
-    public void Print()
+    override public void Print()
     {
         Console.WriteLine("Прямоугольник\n"+this.ToString());
     }
 
 }
-class Square : Rectangle, IPrint
+class Square : Rectangle, ICreate
 {
     
     public Square(double side): base(side,side)
@@ -88,12 +103,12 @@ class Square : Rectangle, IPrint
     {
         return "Сторона: " + this.height.ToString() + " Площадь: " + this.Area().ToString();  
     }
-    public void Print()
+    override public void Print()
     {
         Console.WriteLine("Квадрат\n" + this.ToString());
     }
 }
-class Circle : Figure,IPrint
+class Circle : Figure, ICreate
 {
 
     public void Create()
@@ -115,7 +130,7 @@ class Circle : Figure,IPrint
     {
         return "Радиус: " + this.radius.ToString() + " Площадь: " + this.Area().ToString();
     }
-    public void Print()
+    public override void Print()
     {
         Console.WriteLine("Круг\n" + this.ToString());
     }
@@ -132,10 +147,47 @@ class Program
 
     static int Main()
     {
-       
-        Square s1 = new Square(13.65);
-        Circle c1 = new Circle(6);
 
+        Figure r1 = new Rectangle(5, 10);
+        Figure s1 = new Square(2);
+        Figure s2 = new Square(9);
+        Figure c1 = new Circle(3);
+        Figure r2 = new Rectangle(7, 2);
+
+        List<Figure> figures = new List<Figure>(){ r1, s1, r2, s2, c1 };
+        ArrayList list = new ArrayList() {r1,s1,r2,s2,c1};
+      
+
+        list.Sort(new FigureComparer());
+
+        figures.Sort();
+        foreach(Figure item in list)
+        {
+            item.Print();
+        }
+
+        Cube cube = new Cube(6);
+        cube.PrintMatrix();
+
+        SparseMatrix sparseMatrix = new SparseMatrix();
+        sparseMatrix.SetValue(2, 4, 1, 1);
+        sparseMatrix.SetValue(2, 1, 1, 1);
+
+        Console.WriteLine("Разреженная матрица:");
+        Console.WriteLine(sparseMatrix.ToString());
+
+        SimpleStack<Figure> stack = new SimpleStack<Figure>();
+        stack.Push(r1);
+        stack.Push(s1);
+        stack.Push(c1);
+        stack.Pop();
+
+        return 0;
+       
+    }
+
+    static void interactive()
+    {
         while (true)
         {
             switch (Menu())
@@ -157,7 +209,7 @@ class Program
                     c.Print();
                     break;
                 case 4:
-                    return 0;
+                    return ;
                 default:
                     break;
 
@@ -165,7 +217,6 @@ class Program
 
         }
     }
-
     static double Menu()
     {
         Console.WriteLine("\nКакую фигуры вы ходите создать? ");
@@ -179,3 +230,112 @@ class Program
         return 0;
     }
 }
+
+public class FigureComparer : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        Figure figure1 = x as Figure;
+        Figure figure2 = y as Figure;
+
+        if (figure1 == null || figure2 == null)
+            throw new ArgumentException();
+
+        return figure1.Area().CompareTo(figure2.Area());
+    }
+}
+
+class SparseMatrix
+{
+    private Dictionary<(int x, int y, int z), double> matrix;
+
+    public SparseMatrix()
+    {
+        matrix = new Dictionary<(int, int, int), double>();
+    }
+
+    public void SetValue(int x, int y, int z, double value)
+    {
+        if (value != 0)
+        {
+            matrix[(x, y, z)] = value;
+        }
+        else
+        {
+            matrix.Remove((x, y, z));
+        }
+    }
+
+    public double GetValue(int x, int y, int z)
+    {
+        matrix.TryGetValue((x, y, z), out double value);
+        return value;
+    }
+    public override string ToString()
+    {
+        if (matrix.Count == 0)
+            return "Разреженная матрица пуста.";
+
+        var result = "\n";
+        foreach (var item in matrix)
+        {
+            result += $"Координаты: ({item.Key.x}, {item.Key.y}, {item.Key.z}), Значение: {item.Value}\n";
+        }
+        return result;
+    }
+}
+
+public class Cube
+{
+    public int Size { get; set; }
+    private SparseMatrix matrix;
+
+    public Cube(int size)
+    {
+        Size = size;
+        matrix = new SparseMatrix();
+        InitMatrix();
+    }
+
+
+    private void InitMatrix()
+    {
+        matrix.SetValue(0, 0, 0, 1.0);
+        matrix.SetValue(Size, 0, 0, 1.0);
+        matrix.SetValue(Size, Size, 0, 1.0);
+        matrix.SetValue(0, Size, 0, 1.0);
+        matrix.SetValue(0, 0, Size, 1.0);
+        matrix.SetValue(Size, 0, Size, 1.0);
+        matrix.SetValue(Size, Size, Size, 1.0);
+        matrix.SetValue(0, Size, Size, 1.0);
+    }
+
+
+    public void PrintMatrix()
+    {
+        Console.WriteLine("\nВершины куба:");
+        Console.WriteLine(matrix.ToString());
+    }
+}
+
+class SimpleStack<T> 
+{
+    private LinkedList<T> list = new LinkedList<T>();
+
+    public T Pop()
+    {
+        if (list.Count == 0)
+        {
+            throw new InvalidOperationException("пустой стек"); //проверка на пустой стек
+        }
+
+        T element = list.First.Value;
+        list.RemoveFirst();
+        return element;
+    }
+
+    public void Push(T element)
+    {
+        list.AddFirst(element);
+    }
+};
--- Lab_2/obj/Debug/net8.0/Lab_2.AssemblyInfo.cs
+++ Lab_2/obj/Debug/net8.0/Lab_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdce4e3abcec6e6db94d6522e44e54b5d13a766e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4527e674fe3ec462d4d9cb5c75e0c000298c6ff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
